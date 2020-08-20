fa30e83b25f975b11aa0e5e6e05a61205bc0d3e7
Louis <35883445+louist103@users.noreply.github.com>
2020-08-19 20:11:41 -0400

    Finish decomp.  1 Nonmatching 1 NON_EQUIVALENT

--- src/overlays/actors/ovl_Bg_Ydan_Maruta/z_bg_ydan_maruta.c
+++ src/overlays/actors/ovl_Bg_Ydan_Maruta/z_bg_ydan_maruta.c
@@ -51,15 +51,16 @@ static InitChainEntry sInitChain[] = {
 };
 
 extern UNK_TYPE D_060066A8;
+extern Gfx D_06008D88[];
+extern Gfx D_06006570[];
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/BgYdanMaruta_Init.s")
-#ifdef NON_MATCHING
+//#ifdef NON_MATCHING
 void BgYdanMaruta_Init(Actor* thisx, GlobalContext* globalCtx) {
    s32 i;
    BgYdanMaruta *this = (BgYdanMaruta *) thisx;
    Vec3f sp4C[3];
    s32 pad;
-
    f32 sinRotY;
    f32 cosRotY;
    s32 localConst = 0;
@@ -69,7 +70,7 @@ void BgYdanMaruta_Init(Actor* thisx, GlobalContext* globalCtx) {
     Collider_InitTris(globalCtx, &this->collider);
     Collider_SetTris(globalCtx, &this->collider, thisx, &sTrisInit, &this->items);
     this->unk_168 = thisx->params;
-    thisx->params = ((thisx->params >> 8) & 0xFFu) & 0xFF;
+    thisx->params = ((thisx->params >> 8) & 0xFF) & 0xFF;
     items = &sTrisItemsInit[1];
     if (thisx->params == 0) {
         items = &sTrisItemsInit[0];
@@ -95,18 +96,23 @@ void BgYdanMaruta_Init(Actor* thisx, GlobalContext* globalCtx) {
     }
     func_800627A0(&this->collider, 0, &sp4C[0], &sp4C[1], &sp4C[2]);
     sp4C[1].x = (items->dim.vtx[2].x * cosRotY) + thisx->posRot.pos.x;
-    sp4C[1].y = items->dim.vtx[1].y + thisx->posRot.pos.y;
+    sp4C[1].y = items->dim.vtx[0].y + thisx->posRot.pos.y;
     sp4C[1].z = thisx->posRot.pos.z - (items->dim.vtx[2].x * sinRotY);
     func_800627A0(&this->collider, 1,  &sp4C[0], &sp4C[2], &sp4C[1]);
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/BgYdanMaruta_Init.s")
-#endif
+//#else
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/BgYdanMaruta_Init.s")
+//#endif
 
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/BgYdanMaruta_Destroy.s")
+void BgYdanMaruta_Destroy(Actor *thisx, GlobalContext *globalCtx) {
+    BgYdanMaruta* this = THIS;
+    Collider_DestroyTris(globalCtx, &this->collider);
+    if (thisx->params == 1) {
+        DynaPolyInfo_Free(globalCtx, &globalCtx->colCtx.dyna, this->dyna.dynaPolyId);
+    }
+}
 
-//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/func_808BEFF4.s")
 void func_808BEFF4(BgYdanMaruta *this, GlobalContext *globalCtx) {
     if ((this->collider.base.atFlags & 2) != 0) {
         func_8002F71C(globalCtx, &this->dyna.actor, 7.0f, this->dyna.actor.shape.rot.y, 6.0f);
@@ -116,7 +122,6 @@ void func_808BEFF4(BgYdanMaruta *this, GlobalContext *globalCtx) {
     func_8002F974(&this->dyna.actor, NA_SE_EV_TOGE_STICK_ROLLING - SFX_FLAG);
 }
 
-//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/func_808BF078.s")
 void func_808BF078(BgYdanMaruta *this, GlobalContext *globalCtx) {
  //   u8 temp_a1;
     if ((this->collider.base.acFlags & 2) != 0) {
@@ -131,7 +136,7 @@ void func_808BF078(BgYdanMaruta *this, GlobalContext *globalCtx) {
     }
 }
 
-//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/func_808BF108.s")
+#ifdef NON_EQUIVALENT
 void func_808BF108(BgYdanMaruta *this, GlobalContext *globalCtx) {
     s16 phi_v0;
     if (this->unk_16A != 0) {
@@ -140,29 +145,48 @@ void func_808BF108(BgYdanMaruta *this, GlobalContext *globalCtx) {
     if (this->unk_16A == 0) {
         this->actionFunc = func_808BF1EC;
     }
+
     phi_v0 = this->unk_16A;
-    if ((phi_v0 < 0)  && ((phi_v0 & 3) != 0)) {
+    
+    if ((phi_v0 < 0)  && ((phi_v0 % 4) != 0)) {
         phi_v0 -= 4;
     }
-    (phi_v0 -=  2);
     
+    phi_v0 -= 2;
     if (phi_v0 ==  -2) {
        phi_v0 =  0;
     } else {
         phi_v0 *=  2;
     }
-    this->dyna.actor.posRot.pos.x = ((Math_Coss(this->dyna.actor.shape.rot.y) * phi_v0) + this->dyna.actor.initPosRot.pos.x);
-    this->dyna.actor.posRot.pos.z = ((Math_Sins(this->dyna.actor.shape.rot.y) * phi_v0) + this->dyna.actor.initPosRot.pos.z);
+    this->dyna.actor.posRot.pos.x = (Math_Coss(this->dyna.actor.shape.rot.y) * phi_v0) + this->dyna.actor.initPosRot.pos.x;
+    this->dyna.actor.posRot.pos.z = (Math_Sins(this->dyna.actor.shape.rot.y) * phi_v0) + this->dyna.actor.initPosRot.pos.z;
     func_8002F974(&this->dyna.actor, NA_SE_EV_TRAP_OBJ_SLIDE - SFX_FLAG);
 }
+#else
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/func_808BF108.s")
+#endif
 
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/func_808BF1EC.s")
+void func_808BF1EC(BgYdanMaruta *this, GlobalContext *globalCtx) {
+    this->dyna.actor.velocity.y += 1.0f;
+    if (Math_ApproxF(&this->dyna.actor.posRot.pos.y, this->dyna.actor.initPosRot.pos.y, this->dyna.actor.velocity.y) != 0) {
+        Audio_PlayActorSound2(&this->dyna.actor, NA_SE_EV_LADDER_DOUND);
+        this->actionFunc = func_808BF25C;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/func_808BF1EC.s")
-
-//#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/func_808BF25C.s")
 void func_808BF25C(BgYdanMaruta *this, GlobalContext *globalCtx) {
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/BgYdanMaruta_Update.s")
+void BgYdanMaruta_Update(Actor *thisx, GlobalContext *globalCtx) {
+    BgYdanMaruta* this = THIS;
+    this->actionFunc(this,globalCtx);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Ydan_Maruta/BgYdanMaruta_Draw.s")
+void BgYdanMaruta_Draw(Actor *thisx, GlobalContext *globalCtx) {
+    if (thisx->params == 0) {
+        Gfx_DrawDListOpa(globalCtx, D_06008D88);
+    } else{
+        Gfx_DrawDListOpa(globalCtx, D_06006570);
+    }
+}
